53313f7ace464da7a3e6e145aee6ee54be2b0a8e
Maxim Skakun <maxim.skakun@playtech.com>
2026-09-10 10:38:21 +0300

    commit

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
-export const PORTAL_HUB2COLOR_MOUNT_FOLDER = '../portal-mui/themes/candy-theme/js/app-out/hub2color';
-export const HUB2COLOR_PUBLIC_PATH = '/core3-qa09_core-theme/js/app-out/hub2color';
+module.exports = {
+    PORTAL_HUB2COLOR_MOUNT_FOLDER : '../portal-mui/themes/candy-theme/js/app-out/hub2color',
+    HUB2COLOR_PUBLIC_PATH : '/core3-qa09_core-theme/js/app-out/hub2color',
 
-export const PORTAL_IMG_MOUNT_FOLDER = '../portal-mui/themes/candy-theme/js/app-out/img';
-export const HUB2COLOR_IMG_PUBLIC_PATH = '/core3-qa09_core-theme/js/app-out/img';
+    PORTAL_IMG_MOUNT_FOLDER : '../portal-mui/themes/candy-theme/js/app-out/img',
+    HUB2COLOR_IMG_PUBLIC_PATH : '/core3-qa09_core-theme/js/app-out/img',
 
-export const PORTAL_CSS_MOUNT_FOLDER = '../portal-mui/themes/candy-theme/css';
-export const HUB2COLOR_CSS_PUBLIC_PATH = '/core3-qa09_core-theme/css';
+    PORTAL_CSS_MOUNT_FOLDER : '../portal-mui/themes/candy-theme/css',
+    HUB2COLOR_CSS_PUBLIC_PATH : '/core3-qa09_core-theme/css',
 
-export const PORTAL_IMAGES_MOUNT_FOLDER = '../portal-mui/themes/candy-theme/images';
-export const HUB2COLOR_IMAGES_PUBLIC_PATH = '/core3-qa09_core-theme/images';
+    PORTAL_IMAGES_MOUNT_FOLDER : '../portal-mui/themes/candy-theme/images',
+    HUB2COLOR_IMAGES_PUBLIC_PATH : '/core3-qa09_core-theme/images',
+};
